d62a9db025140aafa6c338a9d4a1abafa952ac80
Sakibur Rahman <sakibmas@gmail.com>
2021-10-03 11:23:05 +0600

    Update VpnTS.ps1

--- VpnTS.ps1
+++ VpnTS.ps1
@@ -2,6 +2,8 @@ $path = "$home"
 $lpath = "$path\VpnTS-$($env:computername)-$(get-date -format yyyyMMdd-hhmm).txt"
 $zpath = "$path\VpnTS-$($env:computername)-$(get-date -format yyyyMMdd-hhmm).zip"
 $ldir = "C:\Program Files\OpenVPN\log"
+If(Test-path $zpath) {Remove-item $zpath}
+If(Test-path $lpath) {Remove-item $lpath}
 
 nslookup myip.opendns.com resolver1.opendns.com > $lpath
 tracert -h 15 -w 1 vpn.bergerbd.com >> $lpath
@@ -9,20 +11,21 @@ tracert -h 15 -w 1 vpn3.bergerbd.com >> $lpath
 function Get-WuaHistory
 {
  $session = (New-Object -ComObject 'Microsoft.Update.Session')
- $history = $session.QueryHistory("",0,50) | ForEach-Object {
+ $history = $session.QueryHistory("",0,10) | ForEach-Object {
   $Product = $_.Categories | Where-Object {$_.Type -eq 'Product'} | Select-Object -First 1 -ExpandProperty Name
   $_ | Add-Member -MemberType NoteProperty -Value $_.UpdateIdentity.UpdateId -Name UpdateId
   $_ | Add-Member -MemberType NoteProperty -Value $_.UpdateIdentity.RevisionNumber -Name RevisionNumber
   $_ | Add-Member -MemberType NoteProperty -Value $Product -Name Product -PassThru
   Write-Output $_
  }
- $history | Where-Object {![String]::IsNullOrWhiteSpace($_.title)} | fl Date, Title, Product, UpdateId, RevisionNumber
+ $history | Where-Object {![String]::IsNullOrWhiteSpace($_.title)} | select Date, Title, Product, UpdateId, RevisionNumber
 }
 
 Get-WuaHistory >> $lpath
 
 if (test-path -Path $ldir){
- If(Test-path $zpath) {Remove-item $zpath}
- Add-Type -assembly "system.io.compression.filesystem"
- [io.compression.zipfile]::CreateFromDirectory($ldir, $zpath) 
+ Compress-Archive -Path $ldir -DestinationPath $zpath
 }
+
+Compress-Archive -Path $lpath -DestinationPath $zpath -Update
+If(Test-path $lpath) {Remove-item $lpath}
